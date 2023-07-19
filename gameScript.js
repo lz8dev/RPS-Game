5d060ec8edd1edb7d7b0567a0f17714eed92d59e
lz8dev <kv.ramosh@gmail.com>
2023-07-18 20:50:27 -0500

    first java functions to change display options and compare gameplay results

--- gameScript.js
+++ gameScript.js
@@ -102,38 +102,192 @@ deactivate this ellipsis script when the user makes his first move, reactivate w
 
 //setting ellipsis animation
 let count = 0;      
-let intro = document.querySelector('.temporal');
 function ellipsis (){           //change the content of 'temporal' adding the dots for ellipsis
-    intro.textContent = '.' + '.'.repeat(count % 4);
-    count++;
+    let intro = document.querySelector('.temporal');
+    if (intro){
+        intro.textContent = '.' + '.'.repeat(count % 4);
+        count++;
+    };
 };
-setInterval(ellipsis, 300);     //calls the 'ellipsis' function every 500ms, note that the function must be called without (), this calls the function itself and not its result after execution.
+setInterval(ellipsis, 200);     //calls the 'ellipsis' function every 500ms, note that the function must be called without (), this calls the function itself and not its result after execution.
+
+
+// game logic
+let choice; 
+let container = document.getElementById("selectorID");       //assign the container and its content to a variable, when any element inside 'selectorID' is clicked, the function is triggered
+container.addEventListener('click', playMatch);             //the variable is called when an event happens, in this case, click on any option
+
+let comScore = 0,
+    userScore = 0,
+    plays = 0,          //attempt counter
+    result,             //description of the choices
+    result2;            //rounds remaining or game over
+
+
+function playMatch () {                 //assign the messages values looking at the round result
+    if (comScore === 5 || userScore === 5) {
+        compare()
+        comScore > userScore ? result2 = 'GAME OVER! Computer wins the match' : 
+        result2 = 'GAME OVER! You win the match';
+        console.log('Computer: ' + comScore);
+        console.log('you: ' + userScore);
+        console.log(result2);
+        comScore = 0;
+        userScore = 0;
+        plays = 0;
+    } else {
+        compare()
+        plays ++;           
+        result2 = 'Plays remaining: ' + (5 - plays);
+        console.log('Computer: ' + comScore);
+        console.log('you: ' + userScore);
+        console.log(result2);
+    }
+    console.log('+++++++++++++++++++');
+}
+
+function compare() {            //compare the choices and returns the round result
+    let playerSelection = event.target.dataset.cname,
+        computerSelection = getComputerChoice();
+           
+    if (playerSelection === 'Blast Bomb' && computerSelection === 'Energy Shield') {
+        result = `-The ${computerSelection} blocks the ${playerSelection}-`;
+        comScore ++;
+    } else if (playerSelection === 'Blast Bomb' && computerSelection === 'Gamma Ray') {
+        result = `-The ${playerSelection} destroys the ${computerSelection}-`;
+        userScore ++;
+    } else if (playerSelection === 'Energy Shield' && computerSelection === 'Blast Bomb') {
+        result = `-The ${playerSelection} blocks the ${computerSelection}-`;
+        userScore ++;
+    } else if (playerSelection === 'Energy Shield' && computerSelection === 'Gamma Ray') {
+        result = `-The ${computerSelection} pierces the ${playerSelection}-`;
+        comScore ++;
+    } else if (playerSelection === 'Gamma Ray' && computerSelection === 'Energy Shield') {
+        result = `-The ${playerSelection} pierces the ${computerSelection}-`;
+        userScore ++;
+    } else if (playerSelection === 'Gamma Ray' && computerSelection === 'Blast Bomb') {
+        result = `-The ${computerSelection} destroys the ${playerSelection}-`;
+        comScore ++;
+    } else {
+        result = '-Mutual destruction-';
+    };
+    return (result);
+}
+
+function getComputerChoice () {         //random choice between 0 and 2, assign a choice to each number
+    let rdm = Math.floor(Math.random()*3);
+    rdm === 0 ? choice = 'Blast Bomb': 
+    rdm === 1 ? choice = 'Energy Shield': 
+    choice = 'Gamma Ray';
+    return (choice);
+}
 
 
 
+function chgToMain(){
+    let element = document.getElementById('statusID')
+    element.innerHTML = `
+        <div class="startImage" id="lBolt">
+            <img src="img/bolt.png" alt="lBolt">
+        </div>
+        <div class="startTitle">
+            CHOOSE YOUR DEVICE<br><div class="temporal"></div>
+        </div>
+        <div class="startImage" id="rBolt">
+            <img src="img/bolt.png" alt="lBolt">
+        </div>`
+}
+
+function chgToPlay(){
+    let element = document.getElementById('statusID');
+    element.innerHTML = `
+        <div class="selection">
+            <div class="sTitle">
+                STATUS
+            </div>
+            <div class="sDiv" id="statDiv"></div>
+        </div>
+        <div class="score">
+            <div class="sTitle">   
+                SCORE
+            </div>
+            <div class="sDiv" id="scrDiv"></div>
+        </div>`
+}
+
+function chgToThink(){
+    let element = document.getElementById('statDiv');
+    element.innerHTML = `
+        <div class="think">
+            A.I. IS THINKING <br><div class="temporal"></div>
+        </div>`;
+}
+
+function chgToStat(){
+    let element = document.getElementById('statDiv');
+    element.innerHTML = `
+        <div class="division" id="selUser">
+            <div class="marker1">YOUR SELECTION</div>
+            <div class="marker2" id="selImgUser">---</div>
+        </div>
+        <div class="division" id="selCpu">
+            <div class="marker1">A.I. SELECTION</div>
+            <div class="marker2" id="selImgCpu">---</div>
+        </div>`
+}
+
+function chgToScr(){
+    let element = document.getElementById('scrDiv');
+    element.innerHTML = `
+        <div class="division" id="scrUser">
+            <div class="marker1">YOUR SCORE</div>
+            <div class="marker2 image" id="scrImgUser">---</div>
+        </div>
+        <div class="division" id="scrCpu">
+            <div class="marker1">A.I. SCORE</div>
+            <div class="marker2 image" id="scrImgCpu">---</div>
+        </div>`
+}
+
+function userPoints(){
+    let element = document.getElementById('scrImgUser');
+    userScore === 0 ? element.innerHTML = `<img src="img/0p.png">`: 
+    userScore === 1 ? element.innerHTML = `<img src="img/1p.png">`:
+    userScore === 2 ? element.innerHTML = `<img src="img/2p.png">`:
+    userScore === 3 ? element.innerHTML = `<img src="img/3p.png">`:
+    userScore === 4 ? element.innerHTML = `<img src="img/4p.png">`:
+    userScore === 5 ? element.innerHTML = `<img src="img/5p.png">`:
+    element = '---';
+}
+
+function cpuPoints(){
+    let element = document.getElementById('scrImgCpu');
+    comScore === 0 ? element.innerHTML = `<img src="img/0p.png">`: 
+    comScore === 1 ? element.innerHTML = `<img src="img/1p.png">`:
+    comScore === 2 ? element.innerHTML = `<img src="img/2p.png">`:
+    comScore === 3 ? element.innerHTML = `<img src="img/3p.png">`:
+    comScore === 4 ? element.innerHTML = `<img src="img/4p.png">`:
+    comScore === 5 ? element.innerHTML = `<img src="img/5p.png">`:
+    element = '---';
+}
+
+/*<div class="division" id="scrUser">
+            <div class="marker1">USER SCORE</div>
+            <div class="marker2 image" id="scrUserImg">
+                <img src="img/5pf.png">
+            </div>
+        </div>
+
+        <div class="division" id="scrCpu">
+            <div class="marker1">A.I. SCORE</div>
+            <div class="marker2 image" id="scrCpuImg">
+                <img src="img/2p.png">
+            </div>
+        </div>
 
 
-/*function to split the div
-
-const div1 = document.createElement('div');
-const div2 = document.createElement('div');
-
-// Apply CSS styles to make div1 and div2 equal-sized
-div1.style.width = '50%';
-div1.style.height = '100%';
-div2.style.width = '50%';
-div2.style.height = '100%';
-
-// Replace originalDiv with div1
-originalDiv.parentNode.replaceChild(div1, originalDiv);
-
-// Append div2 to the body
-document.body.appendChild(div2);
-
-
-
+/*
 //function to choose randomly an option
-
 let choice; 
 let container = document.getElementById("container");       //assign the container and its content to a variable 
 container.addEventListener('click', playMatch);             //the variable is called when an event happens, in this case, click on any button
@@ -204,5 +358,8 @@ function getComputerChoice () {         //random choice between 0 and 2, assign 
     return (choice)
 }
 
+-The Blast Bomb destroys the Gamma Ray-
+-The Energy Shield blocks the Blast Bomb-
+-The Gamma Ray pierces the Energy Shield-
 
 */
