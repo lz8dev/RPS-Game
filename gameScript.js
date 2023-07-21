76f5f91118dae47010b6ee191d01048953629765
lz8dev <kv.ramosh@gmail.com>
2023-07-21 18:47:51 -0500

    Included flickering to the winner score, pending to manage scale and overflow

--- gameScript.js
+++ gameScript.js
@@ -1,115 +1,87 @@
+//variables
+let comScore = 0,
+    userScore = 0,
+    plays = 0,          //attempt counter
+    result,             //description of the choices
+    result2,           //rounds remaining or game over
+    isPlaying = false;
+
 //hover behaviour unoptimized
-let hovRock = document.getElementById('dev1');
-let hovPapr = document.getElementById('dev2');
-let hovScss = document.getElementById('dev3');
-let bomb = document.getElementById('bomb');
-let hdBomb = document.getElementById('handRock');
-let txBomb = document.getElementById('txBomb');
-let brBomb = document.getElementById('dev1');
-let shield = document.getElementById('shield');
-let hdShield = document.getElementById('handPaper');
-let txShield = document.getElementById('txShield');
-let brShield = document.getElementById('dev2');
-let ray = document.getElementById('ray');
-let hdRay= document.getElementById('handScissors');
-let txRay= document.getElementById('txRay');
-let brRay = document.getElementById('dev3');
+function hoverListener() {
+    let hovRock = document.getElementById('dev1'),
+        hovPapr = document.getElementById('dev2'),
+        hovScss = document.getElementById('dev3'),
+        bomb = document.getElementById('bomb'),
+        hdBomb = document.getElementById('handRock'),
+        txBomb = document.getElementById('txBomb'),
+        brBomb = document.getElementById('dev1'),
+        shield = document.getElementById('shield'),
+        hdShield = document.getElementById('handPaper'),
+        txShield = document.getElementById('txShield'),
+        brShield = document.getElementById('dev2'),
+        ray = document.getElementById('ray'),
+        hdRay= document.getElementById('handScissors'),
+        txRay= document.getElementById('txRay'),    
+        brRay = document.getElementById('dev3');
 
-//applying hover styles
-hovRock.addEventListener('mouseenter', function() {
-    if (!isPlaying) {
-        bomb.innerHTML = '<img src="img/bomb2.png" alt="bomb">';
-        hdBomb.innerHTML = '<img src="img/rock2.png" alt="rock">';
-        txBomb.style.color = '#f700ff';
-        brBomb.style.border = '8px solid #ffffff';
-    };
-});
-hovRock.addEventListener('mouseleave', function() {
-    bomb.innerHTML = '<img src="img/bomb.png" alt="bomb">';
-    hdBomb.innerHTML = '<img src="img/rock.png" alt="rock">';
-    txBomb.style.color = '#3fff00';
-    brBomb.style.border = '4px solid #3fff00';
-});
+    //applying hover styles
+    hovRock.addEventListener('mouseenter', function() {
+        if (!isPlaying) {
+            bomb.innerHTML = '<img src="img/bomb2.png" alt="bomb">';
+            hdBomb.innerHTML = '<img src="img/rock2.png" alt="rock">';
+            txBomb.style.color = '#f700ff';
+            brBomb.style.border = '8px solid #ffffff';
+        };
+    });
+    
+    hovRock.addEventListener('mouseleave', function() {
+        bomb.innerHTML = '<img src="img/bomb.png" alt="bomb">';
+        hdBomb.innerHTML = '<img src="img/rock.png" alt="rock">';
+        txBomb.style.color = '#3fff00';
+        brBomb.style.border = '4px solid #3fff00';
+    });
 
-hovPapr.addEventListener('mouseenter', function() {
-    if (!isPlaying) {
-        shield.innerHTML = '<img src="img/shield2.png" alt="shield">';
-        hdShield.innerHTML = '<img src="img/paper2.png" alt="paper">';
-        txShield.style.color = '#f700ff';
-        brShield.style.border = '8px solid #ffffff';
-    };
-});
+    
 
-hovPapr.addEventListener('mouseleave', function() {
-    shield.innerHTML = '<img src="img/shield.png" alt="shield">';
-    hdShield.innerHTML = '<img src="img/paper.png" alt="paper">';
-    txShield.style.color = '#3fff00';
-    brShield.style.border = '4px solid #3fff00';
-});
+    hovPapr.addEventListener('mouseenter', function() {
+        if (!isPlaying) {
+            shield.innerHTML = '<img src="img/shield2.png" alt="shield">';
+            hdShield.innerHTML = '<img src="img/paper2.png" alt="paper">';
+            txShield.style.color = '#f700ff';
+            brShield.style.border = '8px solid #ffffff';
+        };
+    });
 
-hovScss.addEventListener('mouseenter', function() {
-    if (!isPlaying) {
-        ray.innerHTML = '<img src="img/ray2.png" alt="ray">';
-        hdRay.innerHTML = '<img src="img/scissors2.png" alt="scissors">';
-        txRay.style.color = '#f700ff';
-        brRay.style.border = '8px solid #ffffff';
-    };
-});
+    hovPapr.addEventListener('mouseleave', function() {
+        shield.innerHTML = '<img src="img/shield.png" alt="shield">';
+        hdShield.innerHTML = '<img src="img/paper.png" alt="paper">';
+        txShield.style.color = '#3fff00';
+        brShield.style.border = '4px solid #3fff00';
+    });
 
-hovScss.addEventListener('mouseleave', function() {
-    ray.innerHTML = '<img src="img/ray.png" alt="ray">';
-    hdRay.innerHTML = '<img src="img/scissors.png" alt="scissors">';
-    txRay.style.color = '#3fff00';
-    brRay.style.border = '4px solid #3fff00';
-});
+    hovScss.addEventListener('mouseenter', function() {
+        if (!isPlaying) {
+            ray.innerHTML = '<img src="img/ray2.png" alt="ray">';
+            hdRay.innerHTML = '<img src="img/scissors2.png" alt="scissors">';
+            txRay.style.color = '#f700ff';
+            brRay.style.border = '8px solid #ffffff';
+        };
+    });
 
-/*hover code optimized (assess)
-
-// Define a single event listener on the parent container
-document.querySelector('.selector').addEventListener('mouseenter', function(event) {
-    // Check if the event target has the class 'device'
-    if (event.target.classList.contains('device')) {
-      // Get the necessary elements based on the event target
-      let image = event.target.querySelector('.image');
-      let hdImage = event.target.querySelector('.hdImage');
-      let text = event.target.querySelector('.opText');
-      let border = event.target;
-  
-      // Modify the elements as needed
-      image.innerHTML = '<img src="img/bomb2.png" alt="bomb">';
-      hdImage.innerHTML = '<img src="img/rock2.png" alt="rock">';
-      text.style.color = '#f700ff';
-      border.style.border = '8px solid #ffffff';
-    }
-  });
-  
-  // Define another event listener on the parent container for mouseleave
-  document.querySelector('.selector').addEventListener('mouseleave', function(event) {
-    // Check if the event target has the class 'device'
-    if (event.target.classList.contains('device')) {
-      // Get the necessary elements based on the event target
-      let image = event.target.querySelector('.image');
-      let hdImage = event.target.querySelector('.hdImage');
-      let text = event.target.querySelector('.opText');
-      let border = event.target;
-  
-      // Modify the elements as needed
-      image.innerHTML = '<img src="img/bomb.png" alt="bomb">';
-      hdImage.innerHTML = '<img src="img/rock.png" alt="rock">';
-      text.style.color = '#3fff00';
-      border.style.border = '5px solid #3fff00';
-    }
-  });
-
-*/
-
-
-/*
-deactivate this ellipsis script when the user makes his first move, reactivate when the user clicks on 'play again' (HTML: start status)
-*/
+    hovScss.addEventListener('mouseleave', function() {
+        ray.innerHTML = '<img src="img/ray.png" alt="ray">';
+        hdRay.innerHTML = '<img src="img/scissors.png" alt="scissors">';
+        txRay.style.color = '#3fff00';
+        brRay.style.border = '4px solid #3fff00';
+    });
+};
+hoverListener();
 
 //setting ellipsis animation
-let count = 0;      
+let count = 0,
+    choice,
+    container = document.getElementById("selectorID");       //assign the container and its content to a variable, when any element inside 'selectorID' is clicked, the function is triggered
+
 function ellipsis (){           //change the content of 'temporal' adding the dots for ellipsis
     let intro = document.querySelector('.temporal');
     if (intro){
@@ -119,10 +91,6 @@ function ellipsis (){           //change the content of 'temporal' adding the do
 };
 setInterval(ellipsis, 200);     //calls the 'ellipsis' function every 500ms, note that the function must be called without (), this calls the function itself and not its result after execution.
 
-
-// game logic
-let choice; 
-let container = document.getElementById("selectorID");       //assign the container and its content to a variable, when any element inside 'selectorID' is clicked, the function is triggered
 container.addEventListener('click', blockClick);             //the variable is called when an event happens, in this case, click on any option, without () to avoid calling the playMatche function immediately and passing its return value (undefined) as the callback function to the addEventListener method.
 
 function playMatches(event){
@@ -133,20 +101,13 @@ function playMatches(event){
     }    
 }
 
-let comScore = 0,
-    userScore = 0,
-    plays = 0,          //attempt counter
-    result,             //description of the choices
-    result2,           //rounds remaining or game over
-    isPlaying = false;
-
 function blockClick(event) {
     if (!isPlaying) {
         let device = event.target.closest('.device');
         if (!device) {
         return; // return if the clicked element is not a device
         }
-        isPlaying = true; // Set the flag to block the event listener
+        isPlaying = true; // Set the condition to block the event listener
         playMatch(event);
     }
 }
@@ -158,33 +119,67 @@ function playMatch (event) {     //assign the messages values looking at the rou
             computerSelection = getComputerChoice(),
             winner;
         compare(playerSelection, computerSelection);
+        chgToPlay();
         chgToThink();
             setTimeout(function () {
                 chgToStat();
                 userSel(playerSelection);
                 cpuSel(computerSelection);
                 chgToResult();
-                setTimeout(function () {
+                setTimeout(function(){
                     chgToScr();
                     userPoints(userScore);
                     cpuPoints(comScore);
                     isPlaying = false;
-                }, 1000);
+                    setTimeout(function(){
+                        if (comScore === 5) {   
+                            winner = 'A.I. WINS';    
+                            chgToGmOver(winner);
+                        } else if (userScore === 5){
+                            winner = 'YOU WIN';
+                            chgToGmOver(winner);
+                        } else {
+                            winner = 0;
+                        };
+                        winFlick(winner);
+                    }, 0);
+                }, 2000);
             }, 1000);
             console.log(result, result2, userScore, comScore, playerSelection, computerSelection);
-
-        if (comScore === 5) {   
-            winner = 'A.I.';    
-            chgToGmOver(winner);
-        } else if (userScore === 5){
-            winner = 'YOU';
-            chgToGmOver(winner);
-        };
     }
 }
 
 
-  
+function winFlick(winner) { 
+    let fiveUser = document.getElementById('scrImgUser'),
+        fiveCpu = document.getElementById('scrImgCpu')
+        images = ['img/5p.png', 'img/5pf.png'];
+        counter = 0;
+        
+    if (winner === 'A.I. WINS'){
+        let flickInterval = setInterval(function() {
+            if (counter < 6) { // Flicker three times (6 toggles)
+            fiveCpu.style.backgroundImage = `url(${images[counter % 2]})`;
+            counter++;
+            } else {
+            clearInterval(flickInterval); // Clear the interval after three flickers
+            fiveCpu.style.backgroundImage = `url(${images[1]})`; // Set the background to 5pf.png
+            }
+        }, 200); // Adjust the interval duration (in milliseconds) as desired
+    } else if (winner === 'YOU WIN'){
+        let flickInterval = setInterval(function() {
+            if (counter < 6) { // Flicker three times (6 toggles)
+            fiveUser.style.backgroundImage = `url(${images[counter % 2]})`;
+            counter++;
+            } else {
+            clearInterval(flickInterval); // Clear the interval after three flickers
+            fiveUser.style.backgroundImage = `url(${images[1]})`; // Set the background to 5pf.png
+            }
+        }, 200); // Adjust the interval duration (in milliseconds) as desired 
+    } else {
+        return;
+    }
+}
 
 function compare(playerSelection, computerSelection) {            //compare the choices and returns the round result
     if (playerSelection === 'Blast Bomb' && computerSelection === 'Energy Shield'){
@@ -224,8 +219,6 @@ function getComputerChoice () {         //random choice between 0 and 2, assign 
     choice = 'Gamma Ray';
     return (choice);
 }
-
-
 
 function chgToMain(){
     let element = document.getElementById('statusID')
@@ -310,14 +303,65 @@ function chgToGmOver(who){
     let element = document.getElementById('selectorID');
     element.innerHTML = `
         <div class="think">
-            <div>GAME OVER!</div>
-            <div class="result" id="whoWin">- YOU/A.I. WINS THE BATTLE -</div>
-            <button> - FIGHT AGAIN - </button>
+            <div class="gOver">GAME OVER!</div>
+            <div class="result" id="whoWin"></div>
+            <button id="restart"> - FIGHT AGAIN - </button>
         </div>`
     
-    let wResult = document.getElementById('whoWin');
-    wResult.innerHTML = ` - ${who} WINS THE BATTLE! - `;
-}
+    let wResult = document.getElementById('whoWin'),
+        restart = document.getElementById("restart");
+
+    wResult.innerHTML = ` - ${who} THE BATTLE! - `;
+    restart.addEventListener('click', function(){
+        chgToMain();
+        chgToSel()
+        userScore = 0;
+        comScore = 0;
+    });
+};
+
+function chgToSel(){
+    let element = document.getElementById('selectorID');
+    element.innerHTML = `
+    <div class="selector" id="selectorID">
+    <div class="device" id="dev1" data-cname="Blast Bomb">
+        <div class="option">
+            <div class="opText" id="txBomb">Blast<br>Bomb💣</div>
+            <div class="image hdImage" id="handRock">
+                <img src="img/rock.png" alt="rock">
+            </div>
+        </div>
+        <div class="image" id="bomb">
+            <img src="img/bomb.png" alt="bomb">
+    </div>
+    </div>
+
+    <div class="device" id="dev2" data-cname="Energy Shield">
+        <div class="option">
+            <div class="opText" id="txShield">Energy<br>Shield🛡️</div>
+            <div class="image hdImage" id="handPaper">
+                <img src="img/paper.png" alt="paper">
+            </div>
+        </div>
+        <div class="image" id="shield">
+            <img src="img/shield.png" alt="shield">
+        </div>
+    </div>
+
+    <div class="device" id="dev3" data-cname="Gamma Ray">
+        <div class="option">
+            <div class="opText" id="txRay">Gamma<br>Ray🔫</div>
+            <div class="image hdImage" id="handScissors">
+                <img src="img/scissors.png" alt="scissors">
+            </div>
+        </div>
+        <div class="image" id="ray">
+            <img src="img/ray.png" alt="ray">
+        </div>
+    </div>
+    </div>`;
+    hoverListener();
+};
 
 function userPoints(){
     let element = document.getElementById('scrImgUser');
@@ -326,7 +370,6 @@ function userPoints(){
     userScore === 2 ? element.innerHTML = `<img src="img/2p.png">`:
     userScore === 3 ? element.innerHTML = `<img src="img/3p.png">`:
     userScore === 4 ? element.innerHTML = `<img src="img/4p.png">`:
-    userScore === 5 ? element.innerHTML = `<img src="img/5p.png">`:
     element = '---';
 }
 
@@ -337,7 +380,6 @@ function cpuPoints(){
     comScore === 2 ? element.innerHTML = `<img src="img/2p.png">`:
     comScore === 3 ? element.innerHTML = `<img src="img/3p.png">`:
     comScore === 4 ? element.innerHTML = `<img src="img/4p.png">`:
-    comScore === 5 ? element.innerHTML = `<img src="img/5p.png">`:
     element = '---';
 }
 
@@ -356,100 +398,3 @@ function cpuSel(computerSelection){
     computerSelection === 'Gamma Ray' ? cpu.innerHTML = `<img src="img/ray.png">`:
     cpu = '---';
 }
-
-
-
-
-
-/*<div class="division" id="scrUser">
-            <div class="marker1">USER SCORE</div>
-            <div class="marker2 image" id="scrUserImg">
-                <img src="img/5pf.png">
-            </div>
-        </div>
-
-        <div class="division" id="scrCpu">
-            <div class="marker1">A.I. SCORE</div>
-            <div class="marker2 image" id="scrCpuImg">
-                <img src="img/2p.png">
-            </div>
-        </div>
-
-
-/*
-//function to choose randomly an option
-let choice; 
-let container = document.getElementById("container");       //assign the container and its content to a variable 
-container.addEventListener('click', playMatch);             //the variable is called when an event happens, in this case, click on any button
-let comScore = 0,
-    userScore = 0,
-    plays = 0,          //attempt counter
-    result,             //description of the choices
-    result2;            //rounds remaining or game over
-
-function playMatch () {                 //assign the messages values looking at the round result
-    if (plays === 4) {
-        compare()
-        comScore === userScore ? result2 = 'GAME OVER! It´s a tie!':
-        comScore > userScore ? result2 = 'GAME OVER! Computer wins the match' : 
-        result2 = 'GAME OVER! You win the match';
-        console.log('Computer: ' + comScore);
-        console.log('you: ' + userScore);
-        console.log(result2);
-        comScore = 0;
-        userScore = 0;
-        plays = 0;
-    } else {
-        compare()
-        plays ++;           
-        result2 = 'Plays remaining: ' + (5 - plays);
-        console.log('Computer: ' + comScore);
-        console.log('you: ' + userScore);
-        console.log(result2);
-    }
-    console.log('+++++++++++++++++++');
-   
-}
-
-function compare() {            //compare te choices and returns the round result
-    let playerSelection = event.target.name,
-        computerSelection = getComputerChoice();
-           
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        result = `You lose, ${computerSelection} beats ${playerSelection}`;
-        comScore ++;
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        result = `You win, ${playerSelection} beats ${computerSelection}`;
-        userScore ++;
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        result = `You win, ${playerSelection} beats ${computerSelection}`;
-        userScore ++;
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        result = `You lose, ${computerSelection} beats ${playerSelection}`;
-        comScore ++;
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        result = `You win, ${playerSelection} beats ${computerSelection}`;
-        userScore ++;
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        result = `You lose, ${computerSelection} beats ${playerSelection}`;
-        comScore ++;
-    } else {
-        result = 'Tie'
-    };
-    console.log('--------------');
-    console.log(result);
-}
-
-function getComputerChoice () {         //random choice between 0 and 2, assign a choice to each number
-    let rdm = Math.floor(Math.random()*3);
-    rdm === 0 ? choice = 'rock': 
-    rdm === 1 ? choice = 'paper': 
-    choice = 'scissors';
-    return (choice)
-}
-
--The Blast Bomb destroys the Gamma Ray-
--The Energy Shield blocks the Blast Bomb-
--The Gamma Ray pierces the Energy Shield-
-
-*/
